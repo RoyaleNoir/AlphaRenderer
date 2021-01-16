ba72f29800281e6f2df2d89ca1dc3bc745ff8e9f
RoyaleNoir <royalenoir32@gmail.com>
2021-01-17 01:24:10 +1300

    Added README.txt

---
+++ README.md
@@ -0,0 +1,8 @@
+## TODO (Unordered)
+ * Textures
+   * Cellular Automata
+ * Sound
+ * Sky / Fog
+ * Lightmap
+ * Version number
+ * Flat items
--- src/main/java/me/kay/alpha_renderer/AlphaRenderer.java
+++ src/main/java/me/kay/alpha_renderer/AlphaRenderer.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class AlphaRenderer implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        // Imagine having something funny to put in comments.
     }
 }
