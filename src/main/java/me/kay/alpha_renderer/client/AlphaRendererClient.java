4679c76c39e0a8bb3efb8bb8b2363b5b5b07eeef
RoyaleNoir <royalenoir32@gmail.com>
2021-01-18 15:38:30 +1300

    Feat: Version Text

--- src/main/java/me/kay/alpha_renderer/client/AlphaRendererClient.java
+++ src/main/java/me/kay/alpha_renderer/client/AlphaRendererClient.java
@@ -9,9 +9,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.resource.ResourceManager;
@@ -53,6 +56,11 @@ public class AlphaRendererClient implements ClientModInitializer {
                 if(client.player != null)
                 client.openScreen(new AlphaInventoryScreen(client.player));
             }
+        });
+
+        HudRenderCallback.EVENT.register((matrixStack, delta) -> {
+            TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
+            renderer.drawWithShadow(matrixStack, "Minecraft Alpha v1.0.16_02", 2, 2, 0xffffff);
         });
     }
 }
